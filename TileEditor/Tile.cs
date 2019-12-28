f2e8c8fd484a450dbb8aea695a19f2fdd2ed7a72
EngineerSmith <pasmith.bus@gmail.com>
2019-12-28 20:20:32 +0400

    Added Tile data structure for TileEditor

---
+++ TileEditor/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PeachFox.TileEditor
+{
+    public struct Tile
+    {
+        public System.Drawing.Image Thumbnail;
+        public string Image;
+        public List<int> Quads;
+        public float Time;
+    }
+}
