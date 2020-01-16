cff962262a5e8b0991527ff56f4ca225c2187e55
EngineerSmith <pasmith.bus@gmail.com>
2020-01-16 20:01:34 +0400

    Added random string generator

---
+++ RandomString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace PeachFox
+{
+    public static class RandomString
+    {
+        private static readonly Random _random = new Random();
+        public static string Generate(int length)
+        {
+            const string chars = "abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
